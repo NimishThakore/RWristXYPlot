b92ad150d8a197957f5167ec01a8c0bf5d8a9675
NimishThakore <njt3@case.edu>
2020-11-26 16:44:37 -0500

    PoseNet to plot R wrist X and Y coordinates

---
+++ index.html
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title> PoseNet to extract 2D positions of right wrist over time </title>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.dom.min.js"></script>
+  <script src="https://unpkg.com/ml5@latest/dist/ml5.min.js"></script>
+  
+</head>
+
+<body>
+  <h1>PoseNet to extract 2D positions of right wrist over time</h1>
+  <p id='status'>Loading model...</p>
+  <script src="sketch.js"></script>
+</body>
+
+</html>
---
+++ sketch.js
@@ -0,0 +1,56 @@
+﻿/* ===
+ml5 Example
+PoseNet example using p5.js
+=== */
+
+let video;
+let poseNet;
+let poses = [];
+let startTime;
+
+function setup() {
+  createCanvas(1000, 320);
+  background(0);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+poseNet = ml5.poseNet(video, modelReady);
+poseNet.on("pose", function(results) {
+    poses = results;
+  });
+}
+
+
+function modelReady() {
+  select("#status").html("Model Loaded");
+  startTime = Date.now();
+}
+
+
+
+function draw() {
+  //image(video, 0, 0, width, height);
+  grphRWristVert();
+}
+
+
+// A function to track vertical movements of R wrist against time
+function grphRWristVert(){
+   for (let i = 0; i < poses.length; i += 1) {
+     const pose = poses[i].pose;
+      if (pose.rightWrist.confidence > 0.2) {
+        newtime = Date.now() - startTime;
+        fill(255,255,0);
+	noStroke();
+        ellipse(newtime/10, pose.rightWrist.y/2, 4, 4);
+        fill(0,255,255);
+	noStroke();
+        ellipse(newtime/10, pose.rightWrist.x/2, 4, 4); 
+      }
+      if (Date.now() - startTime > 10000){
+        startTime = Date.now();
+        clear();
+	background(0);
+      }
+    }
+  }
